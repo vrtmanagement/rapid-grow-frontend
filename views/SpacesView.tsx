4f6ae8d088b635cdbfa587c63ef47eec04578781
Rite09 <ritesh.vrt@gmail.com>
2026-06-05 13:29:22 +0530

    taskhub subnav fix

--- views/SpacesView.tsx
+++ views/SpacesView.tsx
@@ -11,7 +11,7 @@ const SpacesView: React.FC<SpacesViewProps> = ({ mode, state, updateState }) => 
   return (
     <div
       ref={controller.taskHubRootRef}
-      className="-mx-16 -mb-16 mt-0 min-h-full overflow-x-hidden space-y-6 px-6 pb-8 pt-0 animate-in fade-in duration-700"
+      className="-mx-16 -mb-16 mt-0 min-h-full overflow-x-clip space-y-6 px-6 pb-8 pt-0 animate-in fade-in duration-700"
     >
       <SpacesHeader
         mode={controller.mode}
